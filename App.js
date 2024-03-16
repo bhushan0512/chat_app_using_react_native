179ba9ff34e100d96fd7d820ee09856e0efbce7f
Bhushan A Bangera <bhushanabangera687@gmail.com>
2024-03-16 23:42:54 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import DevelopersPage from "./components/screens/DevelopersPage";
 
 export default function App() {
+  // return(
+  //   <DevelopersPage/>
+  // )
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Chatter</Text>
+        <Ionicons
+          style={styles.headerText}
+          name="chatbubble-outline"
+        />
+      </View>
+      <Text style={styles.text}>Chat Anyone Anonymously</Text>
+      <Image
+        style={{
+          width: "80%",
+          height: "40%",
+          resizeMode: "",
+          marginTop:"3%",
+          marginBottom:"4%"
+        }}
+        // source={require("./assets/favicon.png")}
+        source={require("./assets/welcome.png")}
+      />
+      <TouchableHighlight
+        onPress={() => {}}
+        underlayColor="#3f37c9"
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        onPress={() => {}}
+        underlayColor="#afb1ff"
+        style={styles.developers}
+      >
+        <Text style={styles.buttonText1}>Meet the developers</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +59,66 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: "#724cf9",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    width: "70%",
+    height: "7%",
+    marginBottom:"2.5%",
+    // elevation:5,
+    backgroundColor: "#724cf9",
+    borderRadius: 20,
+  },
+  developers: {
+    borderWidth: 1,
+    borderColor: "#724cf9",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    width: "70%",
+    height: "7%",
+    // elevation:5,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 22,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 5,
+    color: "white",
+  },
+  buttonText1: {
+    fontSize: 18,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 3,
+    color: "#4361ee",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 28,
+    fontWeight: "bold",
+    letterSpacing: 3,
+    color: "#4361ee",
+  },
+  header: {
+    flexDirection: "row",
+  },
+
+  headerText: {
+    color: "#724cf9",
+    fontSize: "60%",
+    fontWeight: "800",
+    marginLeft: "1%",
+    // textDecorationLine: "underline",
+    letterSpacing: 5,
   },
 });
---
+++ components/screens/DevelopersPage.js
@@ -0,0 +1,221 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+export default function DevelopersPage() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Chatter</Text>
+        <Ionicons style={styles.headerText} name="chatbubble-outline" />
+      </View>
+      <Image
+        style={styles.image}
+        // source={require("./assets/favicon.png")}
+        source={require("../../assets/developer.png")}
+      />
+      <Text style={styles.text}>Meet Our Team</Text>
+
+      <View style={styles.column}>
+        <View style={styles.card}>
+          <Image
+            style={styles.avatar}
+            // source={require("./assets/favicon.png")}
+            source={require("../../assets/male.png")}
+          />
+          <View style={styles.info}>
+            <Text style={styles.name}>Bhushan A Bangera</Text>
+            <Text style={styles.usn}>NNM23MC021</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons
+                style={{
+                  color: "#24292E",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-github"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#0a66c2",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-linkedin"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#C13584",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-instagram"
+              />
+            </View>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Image
+            style={styles.avatar}
+            // source={require("./assets/favicon.png")}
+            source={require("../../assets/male.png")}
+          />
+          <View style={styles.info}>
+            <Text style={styles.name}>Delbin George</Text>
+            <Text style={styles.usn}>NNM23MC0_ _</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons
+                style={{
+                  color: "#24292E",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-github"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#0a66c2",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-linkedin"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#C13584",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-instagram"
+              />
+            </View>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Image
+            style={styles.avatar}
+            // source={require("./assets/favicon.png")}
+            source={require("../../assets/female.png")}
+          />
+          <View style={styles.info}>
+            <Text style={styles.name}>Rakshitha Shetty</Text>
+            <Text style={styles.usn}>NNM23MC_ _ _</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons
+                style={{
+                  color: "#24292E",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-github"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#0a66c2",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-linkedin"
+              />
+
+              <Ionicons
+                style={{
+                  color: "#C13584",
+                  padding: "2%",
+                }}
+                size={"25%"}
+                name="logo-instagram"
+              />
+            </View>
+          </View>
+        </View>
+        <Text style={styles.usn}>Made with ❤️</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  header: {
+    marginTop:"18%",
+    flexDirection: "row",
+  },
+
+  headerText: {
+    color: "#724cf9",
+    fontSize: "60%",
+    fontWeight: "800",
+    marginLeft: "1%",
+    // textDecorationLine: "underline",
+    letterSpacing: 5,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  column: {
+    width: "90%",
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  card: {
+    backgroundColor: "#EBE9FF",
+    width: "90%",
+    height: "25%",
+    marginBottom: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#4361ee",
+    letterSpacing: 1.1,
+    marginBottom: 5,
+  },
+  usn: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#4361ee",
+    letterSpacing: 1,
+    marginBottom: 5,
+  },
+  info: {
+    marginLeft: 20,
+  },
+  socialIcons: {
+    flexDirection: "row",
+  },
+  icon: {
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    letterSpacing: 2,
+    textDecorationLine: "underline",
+    color: "#5F57DA",
+    marginBottom: 20,
+  },
+});
